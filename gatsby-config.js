a9dce0c44ed8ee0fb35d28d13bd6b78f40357fa0
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-15 20:36:53 +0800

    add typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    siteMetadata: {
+        title: 'A Yoga Man | 瑜伽男',
+    },
+    plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-glamor', {
+        resolve: 'gatsby-plugin-typography',
+        options: {
+            pathToConfigModule: 'src/utils/typography'
+        }
+    }],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from 'typography';
+import KirkhamTheme from 'typography-theme-kirkham';
+
+const typography = new Typography(kirkhamTheme);
+
+export default typography;
