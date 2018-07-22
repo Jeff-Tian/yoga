7bce70890703320e7e8be75ff9b0a70503c14449
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-22 21:44:26 +0800

    add 2 markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,17 @@ module.exports = {
     siteMetadata: {
         title: 'A Yoga Man | 瑜伽男',
     },
-    plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-glamor', {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `src`,
-            path: `${__dirname}/src/`,
-        },
-    }, {
-        resolve: 'gatsby-plugin-typography',
-        options: {
-            pathToConfigModule: 'src/utils/typography'
-        }
-    }],
+    plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-glamor', `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        }, {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography'
+            }
+        }],
 }
