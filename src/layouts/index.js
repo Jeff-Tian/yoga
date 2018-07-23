9cfc0c84d15c31354c12975fced12bcf2123392e
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-23 18:17:38 +0800

    auth

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -35,14 +35,12 @@ const Layout = ({children, data}) => (
             </Link>
         </g.Div>
 
-        <div
-            style={{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '0px 1.0875rem 1.45rem',
-                paddingTop: 0,
-            }}
-        >
+        <div style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+        }}>
             {children()}
         </div>
     </div>
