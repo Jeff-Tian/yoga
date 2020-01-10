bb44dbaf90ea27ad54c78d8894ef5302117a5f10
Jeff Tian <jie.tian@unicareer.com>
2020-01-10 19:29:24 +0800

    feat: wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,57 @@
 module.exports = {
-    siteMetadata: {
-        title: 'A Yoga Man | 瑜伽男',
+  siteMetadata: {
+    title: 'A Yoga Man | 瑜伽男',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-glamor',
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
     },
-    plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-glamor', `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `src`,
-                path: `${__dirname}/src/`,
-            },
-        }, {
-            resolve: 'gatsby-plugin-typography',
-            options: {
-                pathToConfigModule: 'src/utils/typography'
-            }
-        }],
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        // I have created a dummy site for us to use with the plugins we discussed
+        baseUrl: 'uniheart.000webhostapp.com',
+        protocol: 'https',
+        hostingWPCOM: false,
+        // We will be using some advanced custom fields
+        useACF: true,
+        acfOptionPageIds: [],
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: 'https://uniheart.000webhostapp.com/',
+          replacementUrl: 'http://localhost:8000',
+        },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        includedRoutes: [
+          '**/categories',
+          '**/posts',
+          '**/pages',
+          '**/media',
+          '**/tags',
+          '**/taxonomies',
+          '**/users',
+        ],
+        excludedRoutes: [],
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
+    `gatsby-plugin-sitemap`,
+  ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,23 @@
-const path = require(`path`);
-const {createFilePath} = require(`gatsby-source-filesystem`);
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({node, getNode, boundActionCreators}) => {
-    const {createNodeField} = boundActionCreators
-    if (node.internal.type === `MarkdownRemark`) {
-        const slug = createFilePath({node, getNode, basePath: `pages`})
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug,
-        })
-    }
-};
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
 
-exports.createPages = ({graphql, boundActionCreators}) => {
-    const {createPage} = boundActionCreators
+exports.createPages = ({ graphql, boundActionCreators, reporter }) => {
+  const { createPage } = boundActionCreators
 
-    return new Promise((resolve, reject) => {
-        graphql(`
+  const markdownPostsPromise = new Promise((resolve, reject) => {
+    graphql(`
       {
         allMarkdownRemark {
           edges {
@@ -29,19 +29,57 @@ exports.createPages = ({graphql, boundActionCreators}) => {
           }
         }
       }
-    `
-        ).then(result => {
-            result.data.allMarkdownRemark.edges.forEach(({node}) => {
-                createPage({
-                    path: node.fields.slug,
-                    component: path.resolve(`./src/templates/blog-post.js`),
-                    context: {
-                        // Data passed to context is available in page queries as GraphQL variables.
-                        slug: node.fields.slug,
-                    },
-                })
-            })
-            resolve()
+    `).then(result => {
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        console.log('node = ', node.fields.slug)
+
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Data passed to context is available in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
         })
+      })
+      resolve()
     })
-};
+  })
+  const wordpressPostsPromise = async () => {
+    const result = await graphql(`
+      {
+        allWordpressPost {
+          edges {
+            node {
+              slug
+              wordpress_id
+            }
+          }
+        }
+      }
+    `)
+
+    if (result.errors) {
+      reporter.panicOnBuild(
+        `Error while running GraphQL query: ${JSON.stringify(result.errors)}`
+      )
+
+      return
+    }
+
+    const posts = result.data.allWordpressPost.edges
+    posts.forEach(post => {
+      console.log('node = ', post.node.slug)
+
+      createPage({
+        path: `post/${post.node.slug}`,
+        component: path.resolve(`./src/templates/wordpress-post.js`),
+        context: {
+          id: post.node.wordpress_id,
+          slug: post.node.wordpress_id,
+        },
+      })
+    })
+  }
+  return Promise.all([markdownPostsPromise, wordpressPostsPromise()])
+}
